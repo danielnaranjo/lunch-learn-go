e2cb43c90dc22691cf4cd7dcccb7058821ba764d
luismasuelli <luis@triplemint.com>
2018-07-20 16:15:38 -0500

    another string example

--- 2.expressions/2.vars.go
+++ 2.expressions/2.vars.go
@@ -5,5 +5,7 @@ import "fmt"
 func main() {
 	yourName := "Luis"
 	yourName += " Masuelli"
-	fmt.Printf("Hello, %v", yourName)
+	fmt.Printf("Hello, %v\n", yourName)
+	fmt.Println(`Lorem Ipsum Dolor
+                    Sit Amet`)
 }
